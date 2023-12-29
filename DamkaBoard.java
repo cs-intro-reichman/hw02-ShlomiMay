5c3ded4b645a607d8cd8a3c6fffa015b4ff1a513
ShlomiMay <154429501+ShlomiMay@users.noreply.github.com>
2023-12-29 18:12:11 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,12 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
-	}
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.print(i % 2 != 0 ? "* " : " *");
+			}
+			System.out.println();
+		}
+    }
 }
