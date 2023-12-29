8205d2c829b0639078d2582cffbed756bca65184
ShlomiMay <154429501+ShlomiMay@users.noreply.github.com>
2023-12-29 17:59:11 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,15 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		int sum = 1;
+		String statement = n + " is a prefect number since " + n + " = 1";
+		for (int d = 2; d <= (n / 2); d++) {
+			if (n % d == 0) {
+				statement += " + " + d;
+				sum += d;
+			}
+		}
+		System.out.println(sum == n ? statement : n + " is not a perfect nomber");
 	}
 }
