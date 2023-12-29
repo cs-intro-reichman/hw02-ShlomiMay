6d2a2c8d4a72d3cf50cbf328cebb636a10e94c13
ShlomiMay <154429501+ShlomiMay@users.noreply.github.com>
2023-12-29 14:34:33 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,11 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int x = Integer.parseInt(args[0]);
+		for (int d = 1; d <= (x / 2); d++) {
+			if (x % d == 0) {
+				System.out.println(d);
+			}
+		} System.out.println(x);
 	}
 }
